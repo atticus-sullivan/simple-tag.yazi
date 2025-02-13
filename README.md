70fc931e0cf9eeff9a96575f1c53c5bcf2529379
Huy Hoang <boydaihungst@gmail.com>
2025-02-13 19:12:01 +0700

    feat: Initial

---
+++ README.md
@@ -0,0 +1,196 @@
+# simple-tag
+
+<!--toc:start-->
+
+- [simple-tag](#simple-tag)
+  - [Requirements](#requirements)
+  - [Installation](#installation)
+    - [Add setup function in `yazi/init.lua`.](#add-setup-function-in-yaziinitlua)
+    - [Add fetcher `yazi.toml`](#add-fetcher-yazitoml)
+    - [Add `keymap.toml`](#add-keymaptoml)
+  - [For developers](#for-developers)
+  <!--toc:end-->
+
+This is a Yazi plugin that add tags to files/folders. Each tag has its own tag key.
+DISCLAIM: This is not mactag, and won't use mactag.
+
+## Requirements
+
+> [!IMPORTANT]
+> Minimum version: yazi v25.2.7.
+
+- [yazi](https://github.com/sxyazi/yazi)
+- Tested on Linux.
+
+## Installation
+
+Install the plugin:
+
+```sh
+ya pack -a boydaihungst/simple-tag
+```
+
+> [!IMPORTANT]
+> Deleted files/folders in Yazi will also have their tags cleared.
+> However, if you delete them using a different file manager or through the command line, then re-create them,
+> all tags will be restored. Using trash also retains tags, and only permanently deleting files/folders will clear the tags.
+
+### Prevews
+
+- Tag icon indicator:
+
+![tag_with_icon](assets/2025-02-13-19-08-27.png)
+
+- Tag text indicator a.k.a tag key
+
+![tag_with_tag_key](assets/2025-02-13-19-06-59.png)
+
+### Add setup function in `yazi/init.lua`.
+
+Prefs is optional but the setup function is required.
+
+```lua
+require("simple-tag"):setup({
+  -- icon,text,hidden indicator.
+  ui_mode = "icon", -- (Optional)
+
+  -- Tag icon. Only show when ui_mode = "icon"
+  icon = "󰚋", -- (Optional)
+
+  -- linemode order: icon/text indicator position if linemode
+  -- More info: https://github.com/sxyazi/yazi/blob/077faacc9a84bb5a06c5a8185a71405b0cb3dc8a/yazi-plugin/preset/components/linemode.lua#L4-L5
+  linemode_order = 500 -- (Optional)
+
+
+  -- You can backup/restore this folder. But don't use same folder in the different OS.
+  -- save_path =  -- full path to save tags folder (Optional)
+  --       - Linux/MacOS: os.getenv("HOME") .. "/.config/yazi/tags"
+  --       - Windows: os.getenv("APPDATA") .. "\\yazi\\config\\tags"
+
+  -- Set colors for tag icon|text
+	colors = { -- (Optional)
+		-- Set this same with value `theme.toml` > [manager] > hovered > reversed
+		-- Default theme use "reversed = true".
+		-- More info: https://github.com/sxyazi/yazi/blob/077faacc9a84bb5a06c5a8185a71405b0cb3dc8a/yazi-config/preset/theme-dark.toml#L25
+		reversed = true, -- (Optional)
+
+		-- color for tag by tag key = "*". More colors: https://yazi-rs.github.io/docs/configuration/theme#types.color
+		["*"] = "green", -- (Optional)
+		-- color for tag by tag key = "a". Hex
+		["a"] = "#e80920", -- (Optional)
+		-- ... any keys
+	},
+})
+```
+
+### Add fetcher `yazi.toml`
+
+Use one of these ways:
+
+```toml
+[plugin]
+
+  fetchers = [
+    { id = "simple-tag", name = "*", run = "simple-tag" },
+    { id = "simple-tag", name = "*/", run = "simple-tag" },
+  ]
+# or
+  prepend_fetchers = [
+    { id = "simple-tag", name = "*", run = "simple-tag" },
+    { id = "simple-tag", name = "*/", run = "simple-tag" },
+  ]
+# or
+  append_fetchers = [
+    { id = "simple-tag", name = "*", run = "simple-tag" },
+    { id = "simple-tag", name = "*/", run = "simple-tag" },
+  ]
+
+```
+
+### Add `keymap.toml`
+
+> [!IMPORTANT]
+> Check overlapped with default keys: https://github.com/sxyazi/yazi/blob/main/yazi-config/preset/keymap-default.toml
+
+Since Yazi selects the first matching key to run, `prepend_keymap` always has a higher priority than default.
+Or you can use `keymap` to replace all other keys
+
+```toml
+[manager]
+  prepend_keymap = [
+    # Tagging plugin
+
+    # Toggle tag with any key (press any key to toggle tag)
+    # This won't show the which_key popup. Simply press any tag key to toggle that tag for selected or hovered files/folders.
+    # If the selected files/folders already have the pressed tag key, the tag will be removed from those files/folders.
+    { on = [ '~' ], run = "plugin simple-tag -- toggle-tag", desc = "Toggle tag with any key (press any key to toggle tag)" },
+
+    # Fixed key = *
+    { on = [ "`" ], run = "plugin simple-tag -- toggle-tag --key=*", desc = "Toggle tag with tag key = *" },
+
+    # Switch tag indicator between icon > tag key > hidden.
+    { on = [ "t", "t" ], run = "plugin simple-tag -- toggle-ui", desc = "Toggle tag indicator (icon > tag key > hidden)" },
+
+    # Fixed tag indicator mode = hidden (Available modes: hidden|icon|text)
+    { on = [ "T", "h" ], run = "plugin simple-tag -- toggle-ui --mode=hidden", desc = "Hide all tags indicator" },
+
+    # Clear all tags from selected or hovered files/folders
+    { on = [ "T", "c" ], run = "plugin simple-tag -- clear", desc = "Remove all tags from selected or hovered files" },
+
+    # Selection Mode:
+    # replace → Replace selected list with tagged files
+    # unite → Combine all files from already selected files and files with selected tag.
+    # intersect → Keep only the overlapping files.
+    # subtract → Deselect files which has selected tag
+    # exclude → Keep only the non-overlapping files.
+    # undo → Undo/Redo selection (5 mode above)
+
+    # Default mode = replace -> replace the whole selection list with tagged files/folders
+    # which_key will popup to choose selection mode, after that you have to press a tag key to select.
+    { on = [ "T", "s" ], run = "plugin simple-tag -- toggle-select", desc = "Selection only tagged files (press any tag key to select)" },
+
+    # Fixed selection mode = unite.
+    # This won't show which_key popup, just press any tag key to select
+    { on = [ "T", "a" ], run = "plugin simple-tag -- toggle-select --mode=unite", desc = "Add tagged files to selection (press any tag key to select)" },
+
+    # Fixed key = q and selection mode = replace
+    { on = [ "T", "q" ], run = "plugin simple-tag -- toggle-select --mode=replace --tag=q", desc = "Selection only files with tag key = q" },
+
+    # Undo/Redo selection (only works with 5 modes above)
+    { on = [ "T", "u" ], run = "plugin simple-tag -- toggle-select --mode=undo", desc = "Undo selection action" },
+]
+```
+
+## For developers
+
+Trigger this plugin programmatically:
+
+```lua
+-- In your plugin:
+  local simple_tag = require("simple-tag")
+-- Available actions: toggle-tag, toggle-ui, clear, toggle-select
+  local action = "toggle-select"
+	local args = ya.quote(action)
+	args = args .. " " .. ya.quote("--mode=unite")
+-- another arguments
+-- args = args .. " " .. ya.quote("--tag=q")
+	ya.manager_emit("plugin", {
+		simple_tag._id,
+		args,
+	})
+
+
+-- Special action: "files-deleted" -> clear all tags from these files/folders
+  local args = ya.quote("files-deleted")
+-- A array of string url
+  local files_to_clear_tags = selected_or_hovered_files()
+  for _, url in ipairs(files_to_clear_tags) do
+	  args = args .. " " .. ya.quote(url)
+  end
+  ya.manager_emit("plugin", {
+		simple_tag._id,
+	  args,
+  })
+
+
+```
---
+++ main.lua
@@ -0,0 +1,563 @@
+--- @since 25.2.7
+
+local PackageName = "simple-tag"
+local M = {}
+
+
+-- stylua: ignore
+local CAND_TAG_KEYS = {
+	-- number + special characters
+	{ on = "0" }, { on = "1" }, { on = "2" }, { on = "3" }, { on = "4" },
+	{ on = "5" }, { on = "6" }, { on = "7" }, { on = "8" }, { on = "9" },
+	{ on = "-" }, { on = "=" }, { on = "!" }, { on = "@" }, { on = "#" },
+	{ on = "$" }, { on = "%" }, { on = "^" }, { on = "&" }, { on = "*" },
+	{ on = "(" }, { on = ")" }, { on = "_" }, { on = "+" }, { on = "`" },
+	{ on = "~" },	{ on = "[" },	{ on = "{" },	{ on = "]" },	{ on = "}" },
+	{ on = "\\" },	{ on = "|" },	{ on = ";" },	{ on = ":" },	{ on = "'" },
+	{ on = "\"" },	{ on = "," },	{ on = "<" },	{ on = "." },	{ on = ">" },
+	{ on = "/" },	{ on = "?" },
+	-- word
+	{ on = "q" }, { on = "w" }, { on = "e" }, { on = "r" }, { on = "t" },
+	{ on = "y" }, { on = "u" }, { on = "i" }, { on = "o" }, { on = "p" },
+	{ on = "a" }, { on = "s" }, { on = "d" }, { on = "f" }, { on = "g" },
+	{ on = "h" }, { on = "j" }, { on = "k" }, { on = "l" }, { on = "z" },
+	{ on = "x" }, { on = "c" }, { on = "v" }, { on = "b" }, { on = "n" },
+	{ on = "m" }
+}
+
+local CAND_MODES = {
+	-- number + special characters
+	{ on = "1", desc = "Select Only tagged files" },
+	{ on = "2", desc = "Select tagged files (Unite mode)" },
+	{ on = "3", desc = "Select tagged files (Subtract mode)" },
+	{ on = "4", desc = "Select tagged files (Intersect mode)" },
+	{ on = "5", desc = "Select tagged files (Exclude mode)" },
+	{ on = "6", desc = "Select tagged files (Undo mode)" },
+}
+local DEFAULT_TAG_ICON = "󰚋"
+local DEFAULT_LINEMODE_ORDER = 500
+
+local STATE_KEY = {
+	ui_mode = "ui_mode",
+	preserve_selected_files = "preserve_selected_files",
+	colors = "colors",
+	save_path = "save_path",
+	tags_database = "tags_database",
+	icon = "icon",
+	linemode_order = "linemode_order",
+}
+
+---@enum UI_MODE
+local UI_MODE = {
+	hidden = "hidden",
+	icon = "icon",
+	text = "text",
+}
+
+local UI_MODE_ORDERED = {
+	UI_MODE.hidden,
+	UI_MODE.icon,
+	UI_MODE.text,
+}
+
+---@enum SELECTION_MODE
+local SELECTION_MODE = {
+	UNITE = "unite",
+	SUBTRACT = "subtract",
+	INTERSECT = "intersect",
+	EXCLUDE = "exclude",
+	UNDO = "undo",
+	REPLACE = "replace",
+}
+
+local set_state = ya.sync(function(state, key, value)
+	state[key] = value
+end)
+
+local get_state = ya.sync(function(state, key)
+	return state[key]
+end)
+
+local function success(s, ...)
+	if not get_state(STATE_KEY.no_notify) then
+		ya.notify({ title = PackageName, content = string.format(s, ...), timeout = 3, level = "info" })
+	end
+end
+
+---@enum NOTIFY_MSG
+local NOTIFY_MSG = {
+	TAG_KEY_INVALID = "Tag key should be a single character",
+}
+
+local function fail(s, ...)
+	ya.notify({ title = PackageName, content = string.format(s, ...), timeout = 3, level = "error" })
+end
+
+---@enum PUBSUB_KIND
+local PUBSUB_KIND = {
+	tags_tbl_changed = PackageName .. "-tags-changed",
+	-- "@" persist state
+	ui_mode_changed = "@" .. PackageName .. "-ui-mode-changed",
+	files_deleted = "delete",
+}
+
+--- broadcast through pub sub to other instances
+---@param _ table state
+---@param pubsub_kind PUBSUB_KIND
+---@param data any
+---@param to number default = 0 to all instances
+local broadcast = ya.sync(function(_, pubsub_kind, data, to)
+	ps.pub_to(to or 0, pubsub_kind, data)
+end)
+
+local function pathJoin(...)
+	-- Detect OS path separator ('\' for Windows, '/' for Unix)
+	local separator = package.config:sub(1, 1)
+	local parts = { ... }
+	local filteredParts = {}
+	-- Remove empty strings or nil values
+	for _, part in ipairs(parts) do
+		if part and part ~= "" then
+			table.insert(filteredParts, part)
+		end
+	end
+	-- Join the remaining parts with the separator
+	local path = table.concat(filteredParts, separator)
+	-- Normalize any double separators (e.g., "folder//file" → "folder/file")
+	path = path:gsub(separator .. "+", separator)
+
+	return path
+end
+
+local function tbl_deep_clone(original)
+	if type(original) ~= "table" then
+		return original
+	end
+
+	local copy = {}
+	for key, value in pairs(original) do
+		copy[tbl_deep_clone(key)] = tbl_deep_clone(value)
+	end
+
+	return copy
+end
+
+-- Helper function: Convert an array into a set (table for fast lookup)
+local function tbl_to_set(array)
+	local set = {}
+	for _, v in ipairs(array) do
+		local _v = tostring(v)
+		set[_v] = true
+	end
+	return set
+end
+
+-- Unite: Combine all unique elements from both arrays
+local function tbl_unite(array1, array2)
+	local set = tbl_to_set(array1)
+	for _, v in ipairs(array2) do
+		local _v = tostring(v)
+		set[_v] = true
+	end
+	local result = {}
+	for k in pairs(set) do
+		table.insert(result, k)
+	end
+	return result
+end
+
+-- Subtract: Remove elements of array2 from array1
+local function tbl_subtract(array1, array2)
+	local set2 = tbl_to_set(array2)
+	local result = {}
+	for _, v in ipairs(array1) do
+		local _v = tostring(v)
+		if not set2[_v] then
+			table.insert(result, _v)
+		end
+	end
+	return result
+end
+
+-- Intersect: Keep only common elements between both arrays
+local function tbl_intersect(array1, array2)
+	local set2 = tbl_to_set(array2)
+	local result = {}
+	for _, v in ipairs(array1) do
+		local _v = tostring(v)
+		if set2[_v] then
+			table.insert(result, _v)
+		end
+	end
+	return result
+end
+
+-- Exclude: Remove common elements, keeping only unique ones from both arrays
+local function tbl_exclude(array1, array2)
+	local set1, set2 = tbl_to_set(array1), tbl_to_set(array2)
+	local result = {}
+
+	for _, v in ipairs(array1) do
+		local _v = tostring(v)
+		if not set2[_v] then
+			table.insert(result, _v)
+		end
+	end
+	for _, v in ipairs(array2) do
+		local _v = tostring(v)
+		if not set1[_v] then
+			table.insert(result, _v)
+		end
+	end
+	return result
+end
+
+local get_cwd = ya.sync(function()
+	return tostring(cx.active.current.cwd)
+end)
+
+---@param tags_tbl string tags database table
+---@return table<{[string]:string[]}
+local function read_tags_tbl(tags_tbl)
+	local save_path = get_state(STATE_KEY.save_path)
+	local tbl_saved_file = pathJoin(save_path, tags_tbl, "tags.json")
+
+	local file = io.open(tbl_saved_file, "r")
+	if file == nil then
+		return {}
+	end
+	local tags_tbl_records_encoded = file:read("*all")
+	file:close()
+	local tag_records = ya.json_decode(tags_tbl_records_encoded)
+	return tag_records
+end
+
+-- tags_db format: { "parent_abs_path a.k.a tags_tbl" = { "filename" = [ "q", "w", ... ] } }
+---@param tags_db table<{[string]: table<{[string]:string[]}>}>
+local function write_tags_db(tags_db)
+	local save_path = get_state(STATE_KEY.save_path)
+	for tags_tbl, tags_tbl_records in pairs(tags_db) do
+		local tags_tbl_save_dir = pathJoin(save_path, tags_tbl)
+		for fname, tags in pairs(tags_tbl_records) do
+			if #tags == 0 then
+				tags_db[tags_tbl][fname] = nil
+			end
+		end
+		if next(tags_db[tags_tbl]) == nil then
+			-- delete mode
+			fs.remove("file", Url(pathJoin(tags_tbl_save_dir, "tags.json")))
+			fs.remove("dir_clean", Url(tags_tbl_save_dir))
+			local tags_parent_tbl = Url(tags_tbl_save_dir):parent()
+			if tags_parent_tbl and tags_parent_tbl ~= save_path then
+				fs.remove("dir_clean", tags_parent_tbl)
+			end
+		else
+			-- create/update mode
+			local _, err_create_save_dir = fs.create("dir_all", Url(tags_tbl_save_dir))
+			if err_create_save_dir then
+				fail("Can't create save tags file: %s", tags_tbl_save_dir)
+				break
+			else
+				local _, err_write_tags_tbl =
+					fs.write(Url(pathJoin(tags_tbl_save_dir, "tags.json")), ya.json_encode(tags_tbl_records))
+				if err_write_tags_tbl then
+					fail("Can't save tags to file: %s", tags_tbl_save_dir)
+				end
+			end
+		end
+		broadcast(PUBSUB_KIND.tags_tbl_changed, tags_tbl)
+	end
+end
+
+local render = ya.sync(function()
+	ya.render()
+end)
+
+function M:fetch(job)
+	local tags_db = get_state(STATE_KEY.tags_database)
+	for _, file in ipairs(job.files) do
+		local tags_tbl = tostring(file.url:parent())
+		if tags_db[tags_tbl] == nil then
+			tags_db[tags_tbl] = read_tags_tbl(tags_tbl)
+		end
+	end
+	set_state(STATE_KEY.tags_database, tags_db)
+	render()
+	return true
+end
+
+function M:setup(opts)
+	local st = self
+	local save_path = pathJoin(
+		(ya.target_family() == "windows" and os.getenv("APPDATA") .. "\\yazi\\config\\tags")
+			or (os.getenv("HOME") .. "/.config/yazi/tags")
+	)
+	st[STATE_KEY.tags_database] = {}
+	st[STATE_KEY.icon] = DEFAULT_TAG_ICON
+	st[STATE_KEY.ui_mode] = UI_MODE.icon
+	st[STATE_KEY.colors] = {}
+	st[STATE_KEY.linemode_order] = DEFAULT_LINEMODE_ORDER
+	if type(opts) == "table" then
+		st[STATE_KEY.ui_mode] = opts.ui_mode or st[STATE_KEY.ui_mode]
+		st[STATE_KEY.save_path] = pathJoin(opts.save_path or save_path)
+		st[STATE_KEY.colors] = opts.colors or st[STATE_KEY.colors]
+		st[STATE_KEY.icon] = opts.icon or st[STATE_KEY.icon]
+		st[STATE_KEY.linemode_order] = tonumber(opts.linemode_order) or st[STATE_KEY.linemode_order]
+	end
+
+	-- render tags
+	Linemode:children_add(function(_self)
+		if st[STATE_KEY.ui_mode] == UI_MODE.hidden then
+			return ""
+		end
+		local tags_tbl = tostring(_self._file.url:parent())
+		local fname = _self._file.name
+		local spans = {}
+		if st[STATE_KEY.tags_database][tags_tbl] and st[STATE_KEY.tags_database][tags_tbl][fname] then
+			-- default true
+			local is_reversed_color = not st[STATE_KEY.colors]
+				or st[STATE_KEY.colors].reversed == nil
+				or st[STATE_KEY.colors].reversed == true
+			local tags = st[STATE_KEY.tags_database][tags_tbl][fname] or {}
+			table.sort(tags)
+			for _, tag in ipairs(tags) do
+				local style = ui.Style()
+				if _self._file:is_hovered() then
+					if is_reversed_color then
+						style:bg(st[STATE_KEY.colors][tag] and st[STATE_KEY.colors][tag] or "reset")
+					else
+						style:fg(st[STATE_KEY.colors][tag] and st[STATE_KEY.colors][tag] or "reset")
+					end
+				else
+					style:fg(st[STATE_KEY.colors][tag] and st[STATE_KEY.colors][tag] or "reset")
+				end
+				if st[STATE_KEY.ui_mode] == UI_MODE.icon then
+					spans[#spans + 1] = ui.Span(" " .. st[STATE_KEY.icon]):style(style)
+				elseif st[STATE_KEY.ui_mode] == UI_MODE.text then
+					spans[#spans + 1] = ui.Span(" " .. tag):style(style):bold()
+				end
+			end
+		end
+		return ui.Line(spans)
+	end, st[STATE_KEY.linemode_order])
+
+	ps.sub(PUBSUB_KIND.files_deleted, function(payload)
+		local args = ya.quote("files-deleted")
+		for _, url in ipairs(payload.urls) do
+			args = args .. " " .. ya.quote(tostring(url))
+		end
+		ya.manager_emit("plugin", {
+			self._id,
+			args,
+		})
+	end)
+
+	ps.sub_remote(PUBSUB_KIND.ui_mode_changed, function(mode)
+		set_state(STATE_KEY.ui_mode, mode)
+		render()
+	end)
+
+	ps.sub_remote(PUBSUB_KIND.tags_tbl_changed, function(tags_tbl)
+		local tags_db = get_state(STATE_KEY.tags_database)
+		if tags_db and tags_tbl and tags_db[tags_tbl] then
+			tags_db[tags_tbl] = read_tags_tbl(tags_tbl)
+			set_state(STATE_KEY.tags_database, tags_db)
+			render()
+		end
+	end)
+end
+
+local selected_files = ya.sync(function()
+	local tab, raw_urls = cx.active, {}
+	for _, u in pairs(tab.selected) do
+		raw_urls[#raw_urls + 1] = tostring(u)
+	end
+	return raw_urls
+end)
+
+local selected_or_hovered_files = ya.sync(function()
+	local tab, raw_urls = cx.active, selected_files()
+	if #raw_urls == 0 and tab.current.hovered then
+		raw_urls[1] = tostring(tab.current.hovered.url)
+	end
+	return raw_urls
+end)
+
+local function toggle_tag(new_tag_key)
+	local tags_db = get_state(STATE_KEY.tags_database)
+	local changed_tags_db = {}
+	for _, raw_url in ipairs(selected_or_hovered_files()) do
+		local url = Url(raw_url)
+		local tags_tbl = tostring(url:parent())
+		local fname = tostring(url:name())
+		if not tags_db[tags_tbl] then
+			tags_db[tags_tbl] = {}
+		end
+		if not tags_db[tags_tbl][fname] then
+			tags_db[tags_tbl][fname] = {}
+		end
+		local tags = tags_db[tags_tbl][fname]
+		local is_removed = false
+
+		-- remove if exist
+		for idx = #tags, 1, -1 do
+			if tags[idx] == new_tag_key then
+				table.remove(tags, idx)
+				is_removed = true
+			end
+		end
+
+		-- add if not exist
+		if not is_removed then
+			table.insert(tags, new_tag_key)
+		end
+		tags_db[tags_tbl][fname] = tags
+		changed_tags_db[tags_tbl] = tags_db[tags_tbl]
+	end
+
+	write_tags_db(changed_tags_db)
+end
+
+local function show_cands_ui_modes()
+	local choice_mode = ya.which({ cands = CAND_MODES })
+	local selected_mode = CAND_MODES[choice_mode].on
+	if not selected_mode then
+		return ""
+	elseif selected_mode == "1" then
+		return SELECTION_MODE.REPLACE
+	elseif selected_mode == "2" then
+		return SELECTION_MODE.UNITE
+	elseif selected_mode == "3" then
+		return SELECTION_MODE.SUBTRACT
+	elseif selected_mode == "4" then
+		return SELECTION_MODE.INTERSECT
+	elseif selected_mode == "5" then
+		return SELECTION_MODE.EXCLUDE
+	elseif selected_mode == "6" then
+		return SELECTION_MODE.UNDO
+	end
+end
+
+function M:entry(job)
+	local action = job.args[1]
+	ya.manager_emit("escape", { visual = true })
+
+	if action == "toggle-tag" then
+		local selected_tag_key = job.args.key
+		if selected_tag_key ~= nil and #tostring(selected_tag_key) ~= 1 then
+			fail(NOTIFY_MSG.TAG_KEY_INVALID)
+			return
+		end
+		if not selected_tag_key then
+			local choice = ya.which({ cands = CAND_TAG_KEYS, silent = true })
+			if not choice then
+				return
+			end
+			selected_tag_key = CAND_TAG_KEYS[choice].on
+		end
+		toggle_tag(selected_tag_key)
+	elseif action == "clear" then
+		local args = ya.quote("files-deleted")
+		local files_to_clear = selected_or_hovered_files()
+		for _, url in ipairs(files_to_clear) do
+			args = args .. " " .. ya.quote(url)
+		end
+		ya.manager_emit("plugin", {
+			get_state("_id"),
+			args,
+		})
+	elseif action == "toggle-ui" then
+		local ui_mode = job.args.mode
+		-- toggle between show icons/text keys/hidden
+		if not ui_mode then
+			local old_ui_mode = get_state(STATE_KEY.ui_mode)
+			for idx, m in ipairs(UI_MODE_ORDERED) do
+				if m == old_ui_mode then
+					ui_mode = UI_MODE_ORDERED[idx + 1 > #UI_MODE_ORDERED and 1 or idx + 1]
+					break
+				end
+			end
+		end
+		broadcast(PUBSUB_KIND.ui_mode_changed, ui_mode)
+	elseif action == "toggle-select" then
+		local select_mode = job.args.mode or SELECTION_MODE.REPLACE
+		local selected_tag_key = job.args.tag
+		local new_selected_files = {}
+		-- Mode undo
+		if select_mode == SELECTION_MODE.UNDO then
+			new_selected_files = get_state(STATE_KEY.preserve_selected_files) or {}
+			if new_selected_files == nil then
+				return
+			end
+			local preseve_selected_files = selected_files()
+			set_state(STATE_KEY.preserve_selected_files, preseve_selected_files)
+		else
+			if not selected_tag_key then
+				local choice = ya.which({ cands = CAND_TAG_KEYS, silent = true })
+				if not choice then
+					return
+				end
+				selected_tag_key = CAND_TAG_KEYS[choice].on
+			end
+			if not select_mode then
+				select_mode = show_cands_ui_modes()
+				if not select_mode then
+					return
+				end
+			end
+			local tags_tbl = get_cwd()
+			local tags_db = get_state(STATE_KEY.tags_database)
+			local tagged_filenames = tags_db[tags_tbl] or {}
+			for fname, tags in pairs(tagged_filenames) do
+				for _, tag in ipairs(tags) do
+					if tag == selected_tag_key then
+						table.insert(new_selected_files, pathJoin(tags_tbl, fname))
+						break
+					end
+				end
+			end
+			local preseve_selected_files = selected_files()
+			set_state(STATE_KEY.preserve_selected_files, preseve_selected_files)
+			local old_selected_files = tbl_deep_clone(preseve_selected_files)
+			if select_mode == SELECTION_MODE.REPLACE then
+				-- no needs to do anything else
+			elseif select_mode == SELECTION_MODE.UNITE then
+				new_selected_files = tbl_unite(old_selected_files, new_selected_files)
+			elseif select_mode == SELECTION_MODE.INTERSECT then
+				new_selected_files = tbl_intersect(old_selected_files, new_selected_files)
+			elseif select_mode == SELECTION_MODE.SUBTRACT then
+				new_selected_files = tbl_subtract(old_selected_files, new_selected_files)
+			elseif select_mode == SELECTION_MODE.EXCLUDE then
+				new_selected_files = tbl_exclude(old_selected_files, new_selected_files)
+			else
+				return
+			end
+		end
+
+		-- clear selection
+		ya.manager_emit("escape", { select = true })
+		for _, url in ipairs(new_selected_files) do
+			ya.manager_emit("toggle", { Url(url), state = "on" })
+		end
+	elseif action == "files-deleted" then
+		-- get changes tags
+		local changed_tags_db = {}
+		local tags_db = get_state(STATE_KEY.tags_database)
+		for idx, raw_url in ipairs(job.args) do
+			local url = Url(raw_url)
+			if idx == 1 or url == nil then
+				goto continue
+			end
+			local tags_tbl = tostring(url:parent())
+			local fname = tostring(url:name())
+			if tags_db and tags_tbl and tags_db[tags_tbl] then
+				tags_db[tags_tbl][fname] = nil
+				changed_tags_db[tags_tbl] = tags_db[tags_tbl]
+			end
+			::continue::
+		end
+		write_tags_db(changed_tags_db)
+	end
+end
+
+return M
