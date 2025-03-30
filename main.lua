8a9e278482f2f6a762887b445bbbe3d734aa0c67
Lukas <60581958+atticus-sullivan@users.noreply.github.com>
2025-03-30 17:30:17 +0200

    try to add option to filter for any tag

--- main.lua
+++ main.lua
@@ -1052,6 +1052,11 @@ function M:entry(job)
 		local inputted_tags = job.args.tags or job.args.tags or job.args.keys or job.args.key
 		local filter_mode = job.args.mode or FILTER_MODE["and"]
 		local input_mode = job.args.input
+		local any_tag = job.args.any_tag
+		if any_tag then
+			inputted_tags = ""
+		end
+
 		local title = "Search tags" .. (filter_mode == FILTER_MODE["or"] and " (or)" or "") .. ":"
 		if not inputted_tags then
 			inputted_tags = show_cands_input_tags(title, input_mode)
@@ -1080,7 +1085,8 @@ function M:entry(job)
 		local files = {}
 		for fname, tags in pairs(tagged_filenames) do
 			if
-				(filter_mode == FILTER_MODE["and"] and tbl_is_subset(filter_tags, tags))
+				any_tag
+				or (filter_mode == FILTER_MODE["and"] and tbl_is_subset(filter_tags, tags))
 				or (filter_mode == FILTER_MODE["or"] and tbl_contains_any(tags, filter_tags))
 			then
 				local url = _cwd:join(fname)
